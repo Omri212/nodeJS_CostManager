8f1cc121df034fb515f476f5a85cd475ce1973a9
Omri212 <omri164@outlook.com>
2025-02-10 09:38:54 +0200

    add idb vanilla

--- routes/api.js
+++ routes/api.js
@@ -17,7 +17,7 @@ router.post('/add', async (req, res) => {
             return res.status(400).json({ error: 'Missing required fields: userid, category, description, sum' });
         }
         if (!['food', 'health', 'housing', 'sport', 'education', ''].includes(category)){
-            return res.status(400).json({ error: 'Category not supported.\n please choose from theis list: food, health, housing, sport, education, ' });
+            return res.status(400).json({ error: 'Category not supported. please choose from theis list: food, health, housing, sport, education, ' });
         }
         // If date is not provided, use the current date and time
         const costDate = date ? new Date(date) : new Date();
